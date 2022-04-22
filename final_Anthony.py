af4d24bfdcc685ca34cfca424e751a0c981c4005
anthonyvizzoni <avizzoni@seas.upenn.edu>
2022-04-22 14:43:32 -0400

    m

--- final_Anthony.py
+++ final_Anthony.py
@@ -53,6 +53,7 @@ if __name__ == "__main__":
     for (name, pose) in detector.get_detections():
          print(name,'\n',pose)
 
+    print("trying to get tag0", detector.get_detections())
     #TODO use inverse and matrix composition to get tags in robot frame
 
 
